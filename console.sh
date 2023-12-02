2f8ce702aa7395a9d9efdf1bb8f85bcb9adbbb0d
cj210 <69451265+cj210@users.noreply.github.com>
2023-12-02 17:50:03 +0530

    Update console.sh

--- console.sh
+++ console.sh
@@ -1,1 +1,12 @@
-
+sudo pacman -Syyu
+sudo pacman -S fish neofetch eza 
+chsh -s $(which fish)
+echo 'cd /usr/share/consolefonts/
+setfont LatGrkCyr-12x33.psfu.gz
+cd
+neofetch' >> ~/.config/fish/config.fish
+alias -s "lsa=eza -abFghHlimM --icons --group-directories-first --git --git-repos"
+alias -s "lst=eza -abFghHlimMT --icons --group-directories-first --git --git-repos"
+alias -s "lsg=eza -abFghHlimM --icons --group-directories-first --git --git-repos --git-ignore"
+alias -s "back=cd $OLDPWD"
+reboot
