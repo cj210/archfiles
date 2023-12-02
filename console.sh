40cebcdea7b1551790ae13aee52b16e1789fbada
cj210 <69451265+cj210@users.noreply.github.com>
2023-12-02 19:12:45 +0530

    Update console.sh

--- console.sh
+++ console.sh
@@ -3,14 +3,6 @@ sudo pacman -S fish neofetch eza
 chsh -s $(which fish)
 touch ~/archfiles/c2
 chmod 755 ~/archfiles/c2
-ls -lah ~/.config/fish
-cat ~/.config/fish/config.fish
-cat <<ETT >>  ~/.config/fish/config.fish
-cd /usr/share/kbd/consolefonts/
-setfont LatGrkCyr-12x33.psfu.gz
-cd
-neofetch
-ETT
 cat <<EOT >> ~/archfiles/c2
 alias -s "lsa=eza -abFghHlimM --icons --group-directories-first --git --git-repos"
 alias -s "lst=eza -abFghHlimMT --icons --group-directories-first --git --git-repos"
@@ -20,6 +12,11 @@ rm ~/archfiles/c2
 exit
 echo "All done -- Please reboot"
 EOT
-echo "Sourcing complete -- Please source c2 in the archfiles folder"
 fish
+echo >>~/.config/fish/config.fish "\
+cd /usr/share/kbd/consolefonts/
+setfont LatGrkCyr-12x33.psfu.gz
+cd
+neofetch"
+echo "Sourcing complete -- Please source c2 in the archfiles folder"
 
