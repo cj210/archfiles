d75ff96adb264cc73872d368d64fb64bac23691c
cj210 <69451265+cj210@users.noreply.github.com>
2023-12-02 18:41:16 +0530

    Update console.sh

--- console.sh
+++ console.sh
@@ -8,13 +8,15 @@ alias -s "lsa=eza -abFghHlimM --icons --group-directories-first --git --git-repo
 alias -s "lst=eza -abFghHlimMT --icons --group-directories-first --git --git-repos"
 alias -s "lsg=eza -abFghHlimM --icons --group-directories-first --git --git-repos --git-ignore"
 alias -s "back=cd $OLDPWD"
-rm ~/archfiles/c2 
+rm ~/archfiles/c2
+bash
 cat <<ETT >>  ~/.config/fish/config.fish
 cd /usr/share/consolefonts/
 setfont LatGrkCyr-12x33.psfu.gz
 cd
 neofetch
 ETT
+exit
 echo "All done -- Please reboot"
 EOT
 echo "Sourcing complete -- Please source c2 in the archfiles folder"
