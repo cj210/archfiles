fff6a33d0ef7a8b30eae3699b6b89e7730849a8c
cj210 <69451265+cj210@users.noreply.github.com>
2023-12-02 18:25:41 +0530

    Update console.sh

--- console.sh
+++ console.sh
@@ -7,12 +7,15 @@ setfont LatGrkCyr-12x33.psfu.gz
 cd
 neofetch
 EOT
+touch ~/archfiles/c2
+chmod 755 ~/archfiles/c2
 cat <<EOT >> ~/archfiles/c2
 alias -s "lsa=eza -abFghHlimM --icons --group-directories-first --git --git-repos"
 alias -s "lst=eza -abFghHlimMT --icons --group-directories-first --git --git-repos"
 alias -s "lsg=eza -abFghHlimM --icons --group-directories-first --git --git-repos --git-ignore"
 alias -s "back=cd $OLDPWD"
-echo "All done -- Please reboot
+rm ~/archfiles/c2 
+echo "All done -- Please reboot"
 EOT
 echo "Sourcing complete -- Please source c2 in the archfiles folder"
 fish
